b4f04822f08569b9ae1492a69669236d1370f68b
vesquicio <victosousa92@gmail.com>
2025-12-22 22:28:39 -0300

    First commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,16 @@
+
+
+export default function FooterSR() {
+
+    return (
+
+        <div className={`flex items-center justify-center h-10 pt-10 pb-10 `}>
+
+        <p>Vesquicio. 2025</p>
+
+
+        </div>
+
+
+    )
+}
